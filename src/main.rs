062cce2452cda39181f349eb7a4c1532573c86c4
Sayan Mallick <snyxmk@gmail.com>
2023-07-09 09:55:44 +0530

    simple routing

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
-use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use tracing::info;
 
-async fn handler() -> Html<&'static str> {
-    info!("handling request...");
-    Html("<p>Hello, World!</p>")
-}
-
-async fn handler_404() -> impl IntoResponse {
-    info!("handling 404...");
-    (StatusCode::NOT_FOUND, "nothing to see here")
-}
+use newswave::routes::{global_404, health_check};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().json().init();
 
-    info!("starting server...");
+    tracing::info!("starting server...");
 
-    let app = Router::new().route("/", get(handler)).fallback(handler_404);
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .fallback(global_404);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    info!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
