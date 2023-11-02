218c8d4bf0b0c0e68d85129757ca500e8203492e
Sayan Mallick <snyxmk@gmail.com>
2023-11-02 21:45:55 +0530

    basic routing

--- subscription/src/lib.rs
+++ subscription/src/lib.rs
@@ -1,14 +1,32 @@
 use anyhow::Result;
+use maud::html;
 use spin_sdk::{
-    http::{Request, Response},
+    http::{Params, Request, Response, Router},
     http_component,
 };
 
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_subscription(req: Request) -> Result<Response> {
-    println!("{:?}", req.headers());
-    Ok(http::Response::builder()
-        .status(200)
-        .body(Some("Hello from the subsription component".into()))?)
+    let mut router = Router::new();
+    router.get("/subscribe/:user", api::hello_user);
+    router.handle(req)
+}
+
+mod api {
+    use super::*;
+
+    // /subscribe/:user
+    pub fn hello_user(_req: Request, params: Params) -> Result<Response> {
+        let user = params.get("user").expect("lemon");
+
+        let markup = html!(
+            p { "Hi," (user) "!"}
+        )
+        .into_string();
+
+        Ok(http::Response::builder()
+            .status(200)
+            .body(Some(markup.into()))?)
+    }
 }
